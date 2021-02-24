92b0a4d744ee2b5d73de1fe283fc42bc52f7ea65
unknown <m9shdey@gmail.com>
2021-02-24 10:28:50 +0530

    Seventh Commit

--- html/javascript/home.js
+++ html/javascript/home.js
@@ -5,17 +5,13 @@
 
 
 $(document).ready(function(){
-    
     $(".dropdown").hover(function(){
         var dropdownMenu = $(this).children(".dropdown-menu");
         if(dropdownMenu.is(":visible")){
             dropdownMenu.parent().toggleClass("open");
         }
     });
-
-    
-
-});     
+});    
 
 
     
